0567558c61c6b3da9a0061c7dbdce19e54e48d94
Austin <austin@Austins-MacBook-Pro.local>
2020-05-05 20:41:57 -0500

    add link to demo project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,18 +4,21 @@ import axios from 'axios';
 
 export default class Projects extends React.Component {
   state = {
-    projects: []
+    projects: [],
   };
 
   componentDidMount() {
-    axios.get(`https://api.github.com/users/austinkrueger/repos`).then(res => {
-      const projects = res.data;
-      this.setState({ projects });
-    });
+    axios
+      .get(`https://api.github.com/users/austinkrueger/repos`)
+      .then((res) => {
+        const projects = res.data;
+        this.setState({ projects });
+      });
   }
 
   render() {
     const project_list = this.state.projects;
+    console.log(project_list);
     for (var i = project_list.length - 1; i >= 0; i--) {
       if (project_list[i].name === 'austinkrueger.github.io') {
         project_list.splice(i, 1);
@@ -23,7 +26,7 @@ export default class Projects extends React.Component {
     }
     return (
       <div className="project-container">
-        {project_list.slice(0, 3).map(project => (
+        {project_list.slice(0, 3).map((project) => (
           <div className="card-row" id={project.id}>
             <div className="card-description">
               <h3>{project.name}</h3>
@@ -38,6 +41,17 @@ export default class Projects extends React.Component {
               </p>
               <p>{project.description}</p>
               <h5>Language Used: {project.language || 'N/A'}</h5>
+              {project.homepage && (
+                <p>
+                  <a
+                    href={project.homepage}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View the demo
+                  </a>
+                </p>
+              )}
             </div>
           </div>
         ))}
