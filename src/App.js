1b01e483e8be5f1b36d6fd32ca3a06c89cd69424
Austin <austin@Austins-MacBook-Pro.local>
2020-06-10 20:10:26 -0500

    revert layout styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.scss';
-// import Projects from './components/Projects';
+import Projects from './components/Projects';
 import Header from './components/Header';
-// import Main from './components/Main';
-// import Footer from './components/Footer';
-// import About from './components/About';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   return (
     <main className='app-container'>
       <Header />
-      {/* <About />
+      <Main />
+      <About />
       <Projects />
-      <Footer /> */}
+      <Footer />
     </main>
   );
 }
