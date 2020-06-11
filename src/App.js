b3c327ce86e28e5f4055d8a2d8e201b6ccd446e2
Austin <austin@Austins-MacBook-Pro.local>
2020-06-10 19:43:17 -0500

    remove toggle logic

--- src/App.js
+++ src/App.js
@@ -7,29 +7,8 @@ import Header from './components/Header';
 // import About from './components/About';
 
 function App() {
-  // const d = new Date();
-  // const hours = d.getHours();
-  // const night = hours >= 19 || hours <= 7; // between 7pm and 7am
-  // const body = document.querySelector('body');
-  // const toggle = document.getElementById('toggle');
-  // const input = document.getElementById('switch');
-
-  // if (night) {
-  //   input.checked = true;
-  //   body.classList.add('night');
-  // }
-
-  // toggle.addEventListener('click', function () {
-  //   const isChecked = input.checked;
-  //   if (isChecked) {
-  //     body.classList.remove('night');
-  //   } else {
-  //     body.classList.add('night');
-  //   }
-  // });
   return (
     <main className='app-container'>
-      {/* <Switch /> */}
       <Header />
       {/* <About />
       <Projects />
