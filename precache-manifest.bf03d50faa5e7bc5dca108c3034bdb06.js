7f5296c4c9bfdab138e26a4223def6165e0a5be5
Austin <akruegr@gmail.com>
2020-06-16 22:21:23 -0500

    Updates

--- precache-manifest.bf03d50faa5e7bc5dca108c3034bdb06.js
+++ precache-manifest.bf03d50faa5e7bc5dca108c3034bdb06.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d50be7a9f04556923309ad0e0cb7c81",
+    "revision": "9179a6ce1b8cfa096180e066e769e3f8",
     "url": "/index.html"
   },
   {
-    "revision": "da08a55d9f8181118b5d",
+    "revision": "20c0c72026f6c3426110",
     "url": "/static/css/main.313c6379.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.39b4755d.chunk.js"
   },
   {
-    "revision": "da08a55d9f8181118b5d",
-    "url": "/static/js/main.15f9b49d.chunk.js"
+    "revision": "20c0c72026f6c3426110",
+    "url": "/static/js/main.fca8aa3f.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
